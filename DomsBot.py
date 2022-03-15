9d853f0ac484e1429ff45f0205d2ca7df75e2402
Dominik <domgalp@gmail.com>
2022-03-15 17:47:25 +0100

    Void rays

--- DomsBot.py
+++ DomsBot.py
@@ -22,7 +22,12 @@ class DomsBot(BotAI):
         if self.townhalls:
             nexus = self.townhalls.random
 
-            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):
+            # voidray
+            if self.structures(UnitTypeId.VOIDRAY).amount < 10 and self.can_afford(UnitTypeId.VOIDRAY):
+                for sg in self.structures(UnitTypeId.STARGATE).ready.idle:
+                    sg.train(UnitTypeId.VOIDRAY)
+
+            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE) and self.supply_left > 8: # probe
                 nexus.train(UnitTypeId.PROBE)
 
             elif not self.structures(UnitTypeId.PYLON) and self.already_pending(UnitTypeId.PYLON) == 0:
@@ -33,7 +38,14 @@ class DomsBot(BotAI):
                 if self.can_afford(UnitTypeId.PYLON):
                     target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0]) # expand towards enemy
                     pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange(8, 15))
-                    await self.build(UnitTypeId.PYLON, near=pos)
+                    await self.build(UnitTypeId.PYLON, near=nexus)
+
+            elif self.structures(UnitTypeId.ASSIMILATOR).amount < 2: # assimmilator
+                vespenes = self.vespene_geyser.closer_than(15, nexus)
+                for vespene in vespenes:
+                    if self.can_afford(UnitTypeId.ASSIMILATOR) and not self.already_pending(UnitTypeId.ASSIMILATOR):
+                        await self.build(UnitTypeId.ASSIMILATOR, vespene)
+
 
             elif not self.structures(UnitTypeId.FORGE): 
                 if self.can_afford(UnitTypeId.FORGE):
@@ -43,14 +55,36 @@ class DomsBot(BotAI):
                 if self.can_afford(UnitTypeId.PHOTONCANNON):
                     await self.build(UnitTypeId.PHOTONCANNON, near=nexus)
 
+            buildings = [UnitTypeId.GATEWAY, UnitTypeId.CYBERNETICSCORE, UnitTypeId.STARGATE]
+            for building in buildings:
+                if not self.structures(building) and self.already_pending(building) == 0:
+                    if self.can_afford(building):
+                        await self.build(building, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
+                    break
+
+
         else: 
             if self.can_afford(UnitTypeId.NEXUS):
                 await self.expand_now()
         
+        # attack
+        if self.units(UnitTypeId.VOIDRAY).amount >= 3:
+            if self.enemy_units:
+                for vr in self.units(UnitTypeId.VOIDRAY):
+                    vr.attack(random.choice(self.enemy_units))
+            
+            elif self.enemy_structures:
+                for vr in self.units(UnitTypeId.VOIDRAY).idle:
+                    vr.attack(random.choice(self.enemy_structures))
+
+            else:
+                for vr in self.units(UnitTypeId.VOIDRAY).idle:
+                    vr.attack(self.enemy_start_locations[0])
+            
 
 run_game(
     maps.get("Acolyte LE"),
     [Bot(Race.Protoss, DomsBot()),
-    Computer(Race.Zerg, Difficulty.Hard)],
+    Computer(Race.Protoss, Difficulty.Hard)],
     realtime=False # True
 )
